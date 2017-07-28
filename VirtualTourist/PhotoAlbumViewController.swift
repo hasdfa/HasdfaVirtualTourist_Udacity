bd6bec154756db21e468c295d7935ed74634d3b1
Raksha Vadim <hasdfa@protonmail.com>
2017-07-28 16:36:18 +0300

    PhotoC

---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,130 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Raksha Vadim on 28.07.17.
+//  Copyright © 2017 Raksha Vadim. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class PhotoAlbumViewController: CoreDataViewController {
+
+    var annotation: MKAnnotation? = nil
+    var pin: Pin? = nil
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlow: UICollectionViewFlowLayout!
+    
+    var photos: [Photo] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDelegate()
+        
+        // Get the stack
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let stack = delegate.stack
+        
+        // Create a fetchrequest
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Photo")
+        fr.sortDescriptors = []
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        
+        if self.photos.count == 0 {
+            NetworksHelper.searchByLatLon(annotation!.coordinate.latitude, annotation!.coordinate.longitude, pin: pin, {
+                
+                
+                print("photosCount is ", self.photos.count)
+                
+                DispatchQueue.main.async {
+                    print("loaded")
+                    self.collectionView.reloadData()
+                }
+            })
+        }
+
+        // Setup Flow layout
+        let space:CGFloat = 0.5
+        
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        collectionViewFlow.minimumInteritemSpacing = space
+        collectionViewFlow.minimumLineSpacing = space
+        collectionViewFlow.itemSize = CGSize(width: dimension, height: dimension)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let _2d = CLLocationCoordinate2D(latitude: (pin?.lat)!, longitude: (pin?.lon)!)
+        mapView.addAnnotation(MKPlacemark(coordinate: _2d))
+        mapView.camera.centerCoordinate = _2d
+        mapView.camera.altitude = 1_000_000
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count == 0 ? 21 : photos.count
+    }
+}
+
+// CoreDataVC "delegates"
+extension PhotoAlbumViewController {
+    fileprivate func setupDelegate(){
+        reloadData = {
+            self.collectionView.reloadData()
+        }
+        insertItem = {
+            indexPath -> Void in
+            self.collectionView.insertItems(at: [indexPath])
+        }
+        deleteItem = {
+            indexPath -> Void in
+            self.collectionView.deleteItems(at: [indexPath])
+        }
+        updateItem = {
+            indexPath -> Void in
+            self.collectionView.deleteItems(at: [indexPath])
+            self.collectionView.insertItems(at: [indexPath])
+        }
+        didChangeUpdates = {
+            
+        }
+    }
+}
+
+extension PhotoAlbumViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dCell", for: indexPath) as? PhotoCell
+        
+        if let cell = cell {
+            if photos.count == 0 {
+                cell.progress.startAnimating()
+                cell.layer.borderColor = UIColor.blue.cgColor
+                cell.layer.borderWidth = 3
+            } else {
+                if cell.progress.isAnimating {
+                    cell.progress.stopAnimating()
+                }
+                cell.thumbnail.image = UIImage(data: photos[indexPath.row].imageData! as Data)
+                cell.layer.borderColor = UIColor.clear.cgColor
+                cell.layer.borderWidth = 0
+            }
+        }
+        
+        return cell ?? UICollectionViewCell()
+    }
+    
+}
